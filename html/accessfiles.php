0105d6e4da05324ecebb748f3dcb94504ba7d561
Anne M <ammock1@ilstu.edu>
2020-07-14 11:33:45 -0500

    File structure fix part 1

--- html/accessfiles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mydir = '../file-upload/' . $_SESSION["username"];
-
-//scanning files in a given diretory in ascending order
-$myfiles = scandir($mydir, 1);
-$length = count($myfiles);
-?>
